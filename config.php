bfa45537ea0a7174695340a93c990f36e2ae1be3
Luiz Augusto <augustodia97@gmail.com>
2020-05-07 19:42:25 -0300

    Adicionando o Index.php e Config.php, configurando o BD e separando o HTML na pasta _MODELO.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    $base = 'http://localhost/devsbook';
+
+    $db_name = 'devsbook';
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+
+    $pdo = new PDO("mysql:dbname".$db_name.";host=".$db_host, $db_user, $db_pass);
