ebb38d3372c87eeb4654a533f1185b10058c8c0f
Luiz Augusto <augustodia97@gmail.com>
2020-06-21 21:08:47 -0300

    Adicionando os Partials

--- index.php
+++ index.php
@@ -5,4 +5,13 @@ require 'models/Auth.php';
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
 
-echo 'Index';
+require 'partials/header.php';
+require 'partials/menu.php';
+?>
+<section class="feed mt-10">
+...
+</section>
+
+<?php 
+require 'partials/footer.php';
+?>
